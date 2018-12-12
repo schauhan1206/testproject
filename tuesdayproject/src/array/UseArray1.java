5aa349cae6d9675e35748bc641a6e13b248edad7
seemal <schauhan1206@gmail.com>
2018-12-12 00:09:23 -0500

    commit and push tuesday

---
+++ tuesdayproject/src/array/UseArray1.java
@@ -0,0 +1,22 @@
+package array;
+
+public class UseArray1 {
+
+	public static void main(String[] args) {
+		
+		int []  list = new int[10];
+		
+		list [0] = 5;
+		list [2] = 20;
+		list [4] = 60;
+		System.out.println(list[0]);
+		System.out.println(list[4]);
+		System.out.println(list[2]);
+		
+		
+		
+		
+
+	}
+
+}
