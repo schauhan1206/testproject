3f512b8c64e5f19677b4da8195b5cb965af09ae6
seemal <schauhan1206@gmail.com>
2018-12-15 11:01:12 -0500

    commit and push nested

---
+++ nest/src/nested/NestLoop.java
@@ -0,0 +1,24 @@
+package nested;
+
+public class NestLoop {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your choice for the following multiple questions");
+		for(int i=1; i<10; i++) {
+			System.out.print(i);
+			for(int j=0; j<1; j++) {
+				System.out.print("a.");
+				System.out.print("B");
+				System.out.print("c");
+				System.out.print("d");
+  System.out.println();
+  
+
+				
+					
+			}
+		}
+
+	}
+
+}
