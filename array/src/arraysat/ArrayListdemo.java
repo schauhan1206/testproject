1d7e8b58e0fc8511a296977cd4fdc1bc8c9a41cb
seemal <schauhan1206@gmail.com>
2018-12-16 15:47:12 -0500

    commit and push array

---
+++ array/src/arraysat/ArrayListdemo.java
@@ -0,0 +1,33 @@
+package arraysat;
+import java.util.*;
+public class ArrayListdemo {
+
+	public static void main(String[] args) {
+		ArrayList a1 = new ArrayList();
+		System.out.println("Initial size of a1:"+a1.size());
+		a1.add("C");
+		a1.add("A");
+		a1.add("E");
+		a1.add("B");
+		a1.add("D");
+		a1.add("F");
+		a1.add(1,"A2");
+		System.out.println("Size of a1 after additions:" + a1.size());
+		System.out.println("Contents of a1:"+a1);
+		a1.remove("F");
+		a1.remove(2);
+		System.out.println("Size of a1 after deletions:"+ a1.size());
+		System.out.println("Contents of a1:"+a1);
+		
+		
+
+
+
+
+		
+		
+		
+
+	}
+
+}
